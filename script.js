ea4ac417f75824694b6baa1682a9175463cb71c9
spame978 <zeefjens@gmail.com>
2023-12-18 11:58:49 +0100

    Simple dialogue

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let main_menu = document.getElementById("main-menu");
+let character = document.getElementById("character");
 
 let currentScene;
 let currentSceneId;
@@ -19,22 +20,38 @@ function loadScene(scene) {
   //Load scene
   let sceneElement = document.getElementById("scene-" + scene);
   sceneElement.style.display = "flex";
-  sceneElement.style.backgroundImage = "url('scenes/scene-" + scene + ".jpg')";
+  sceneElement.style.backgroundImage = "url('scenes/scene-" + scene + ".png')";
 
   //Load dialogue
   currentScene = document.getElementById("scene-" + scene);
   currentSceneId = scene;
+  // Uncomment to enable dialogue
   typeWriter(scene);
+  //For now
+  setCharacter("Eamon");
 }
+
+dialogue_scene_3 = {
+  Eamon: "Hello moneymaster!!!!!!",
+  Money: "Hello dickhead",
+};
 
 dialogue_scene_1 = ["Hello moneymaster!!!!!", "Hello shithead"];
 dialogue_scene_2 = ["moneymaster!!!!!", "shwegreewfefithead"];
 
+function setCharacter(personImange) {
+  character.src = "scenes/" + personImange + ".png";
+}
+
 function typeWriter(sceneId) {
+  character.style = "animation: talk-Animation 0.3s infinite;";
   if (index < dialogue_scene_1[0].length) {
-    document.getElementById("dialouge-box").innerHTML +=
+    document.getElementById("dialouge").innerHTML +=
       dialogue_scene_1[0].charAt(index);
     index++;
     setTimeout(typeWriter, speed);
+  } else {
+    console.log("done");
+    character.style = "animation: none;";
   }
 }
