a0b15fb9ac7592f33ed50e6a9bd7d2f7a1b313c3
spame978 <zeefjens@gmail.com>
2023-12-20 11:51:20 +0100

    json for loading dialogue + Rename of characters and Buttons(Disabled untill fixed)

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ let currentSceneId;
 let index = 0;
 let speed = 50;
 
+// Load and cache dialogue.json
+let dialogueJson;
+fetch("./dialogue.json")
+  .then((response) => response.json())
+  .then((json) => {
+    dialogueJson = json;
+  });
+
 function StartGame() {
   //Remove main menu
   main_menu.style.display = "none";
@@ -25,31 +33,38 @@ function loadScene(scene) {
   //Load dialogue
   currentScene = document.getElementById("scene-" + scene);
   currentSceneId = scene;
-  // Uncomment to enable dialogue
-  typeWriter(scene);
-  //For now
-  setCharacter("Eamon");
-}
 
-dialogue_scene_3 = {
-  Eamon: "Hello moneymaster!!!!!!",
-  Money: "Hello dickhead",
-};
+  //For now
+  setCharacter("Lucas");
+
+  character.onload = function () {
+    // Uncomment to enable dialogue
+
+    typeDialogue(scene, true);
+  };
+}
 
 dialogue_scene_1 = ["Hello moneymaster!!!!!", "Hello shithead"];
 dialogue_scene_2 = ["moneymaster!!!!!", "shwegreewfefithead"];
 
-function setCharacter(personImange) {
-  character.src = "scenes/" + personImange + ".png";
+function setCharacter(person) {
+  //Set person image and set the speaking title to the person
+  character.src = "scenes/" + String(person).toLowerCase() + ".png";
+  document.getElementById("person-title").innerHTML = person;
 }
 
-function typeWriter(sceneId) {
-  character.style = "animation: talk-Animation 0.3s infinite;";
+// Type out dialogue
+function typeDialogue(sceneId, shouldMove) {
+  if (shouldMove) {
+    character.style = "animation: talk-Animation 0.3s infinite;";
+  }
+
+  //Loop for every char in the strinng
   if (index < dialogue_scene_1[0].length) {
     document.getElementById("dialouge").innerHTML +=
       dialogue_scene_1[0].charAt(index);
     index++;
-    setTimeout(typeWriter, speed);
+    setTimeout(typeDialogue, speed);
   } else {
     console.log("done");
     character.style = "animation: none;";
